2dff19cf8c171f165ef82b0e9cbe043988ca6c97
paulo <paulo.camuri@outlook.com>
2025-01-29 21:11:21 -0300

    Página com carousel_slider.dart e botoes

---
+++ todolist/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text('Olá Mundo'),
+        ],
+      )),
+    );
+  }
+}
