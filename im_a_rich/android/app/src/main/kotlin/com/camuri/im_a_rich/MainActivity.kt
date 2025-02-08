1a01ad3d76cb7f18ebc4d704aeb9d8e7fd8ff0f1
paulo <paulo.camuri@outlook.com>
2025-02-08 10:35:46 -0300

    tema claro/escuro

---
+++ im_a_rich/android/app/src/main/kotlin/com/camuri/im_a_rich/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.camuri.im_a_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ im_a_rich/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('Im rich'),
+        ),
+      ),
+    ),
+  );
+}
+
+
